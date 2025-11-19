387d14aef41382f9e82f7dfcb3bcae0fcfea12ab
leardrama <leard.ramq@gmail.com>
2025-11-19 19:36:14 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN 
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
